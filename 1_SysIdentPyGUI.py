295c3f5b9afc82b6749c78b2446ea3ba2fd00725
jceneziojr <julio.cesar.enezio@gmail.com>
2023-03-21 17:45:54 -0300

    minor change

--- 1_SysIdentPyGUI.py
+++ 1_SysIdentPyGUI.py
@@ -247,7 +247,7 @@ with tab2:
                         model_struc_selec_par_dict['random_state'] = st.session_state['random_statemss']
 
         st.markdown("""---""")
-        st.write(model_struc_selec_par_dict)
+
         model_struc_selec_module = importlib.import_module('sysidentpy.model_structure_selection'+'.'+model_struc_dict[st.session_state['model_struc_select_key']][0])
         model = utils.str_to_class(model_struc_dict[st.session_state['model_struc_select_key']][1], model_struc_selec_module)(**model_struc_selec_par_dict)
 
